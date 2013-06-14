c0196c9bc2499dc5230c51c798c353715ab8694c
Yeguang Xue <blackholexyg@gmail.com>
2013-06-14 20:49:57 +0800

    online

--- online.m
+++ online.m
@@ -13,11 +13,24 @@ function [flag]= online(X,Y,P1,P2)
 % Output:       flag: 1  - on line segment
 %                     -1 - out of line segment
 %
-flag = 1;
-
-if X > max (P1(1),P2(1))|| X < min (P1(1),P2(1)) || Y > max (P1(2),P2(2))|| Y < min (P1(2),P2(2))
-    flag = 0;
-    return;
-end
-    
+flag = 0;
+if P1(1)>P2(1)
+    if X>=P2(1)&&X<P1(1)
+        flag=1;
+    end
+elseif P1(1)==P2(1)
+    if P1(2)>P2(2)
+        if Y>=P2(2)&&Y<P1(2)
+            flag=1;
+        end
+    else
+        if Y>P1(2)&&Y<=P2(2)
+            flag=1;
+        end
+    end
+else
+    if X>P1(1)&&X<=P2(1)
+        flag=1;
+    end
+end    
 end
